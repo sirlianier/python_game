62605a4b5d0227ea1ee36e5fc5f208239b32f892
Sirli Anier <sirli.anier.001@khk.ee>
2021-04-06 17:01:14 +0100

    Defined game settings file

--- game.py
+++ game.py
@@ -1,13 +1,14 @@
 import sys
 import pygame
+from settings import Settings
 
 def run_game():
     # init game and create display object
     pygame.init()
-    screen = pygame.display.set_mode((800, 600))
+    game_settings = Settings()
+    screen = pygame.display.set_mode((game_settings.screen_width, game_settings.screen_height))
     pygame.display.set_caption("Example Game")
     # backround color
-    bg_color = (238,238,228)
 
     while True:
         # control keyboard and mouse events
@@ -15,7 +16,7 @@ def run_game():
             if event.type == pygame.QUIT:
                 sys.exit()
         # add screen backround
-        screen.fill(bg_color)
+        screen.fill(game_settings.bg_color)
         # display the last screen
         pygame.display.flip()
 # test game
---
+++ settings.py
@@ -0,0 +1,9 @@
+class Settings:
+    """"A class to store all setting for example game"""
+
+    def __init__(self):
+        """"Initialize the game settings"""
+        # screen settings
+        self.screen_width = 800
+        self.screen_height = 600
+        self.bg_color = (238,38,228)
