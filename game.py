8ca2724da5c9d507712a78d1caf712326fff10b5
Sirli Anier <sirli.anier.001@khk.ee>
2021-04-06 17:53:53 +0100

    Display ship on screen

--- game.py
+++ game.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
 
 def run_game():
     # init game and create display object
@@ -8,7 +9,9 @@ def run_game():
     game_settings = Settings()
     screen = pygame.display.set_mode((game_settings.screen_width, game_settings.screen_height))
     pygame.display.set_caption("Example Game")
-    # backround color
+
+    #create ship
+    ship = Ship(screen)
 
     while True:
         # control keyboard and mouse events
@@ -17,6 +20,8 @@ def run_game():
                 sys.exit()
         # add screen backround
         screen.fill(game_settings.bg_color)
+        # add ship to screen
+        ship.blitme()
         # display the last screen
         pygame.display.flip()
 # test game
