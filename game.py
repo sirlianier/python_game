2df592ed99d157350943493752b9076732e09dad
Sirli Anier <sirli.anier.001@khk.ee>
2021-04-06 12:48:30 +0100

    Defined backround color

--- game.py
+++ game.py
@@ -6,12 +6,16 @@ def run_game():
     pygame.init()
     screen = pygame.display.set_mode((800, 600))
     pygame.display.set_caption("Example Game")
+    # backround color
+    bg_color = (238,238,228)
 
     while True:
         # control keyboard and mouse events
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+        # add screen backround
+        screen.fill(bg_color)
         # display the last screen
         pygame.display.flip()
 # test game
